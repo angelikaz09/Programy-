67753a02fe3fa1599f17a7d0e7b21a0aa70a266c
angelikaz09 <angelika904@wp.pl>
2017-11-28 13:31:04 +0100

    fibonacci

---
+++ Python/fibonacci.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def fib_iter(n):
+    """funkcja wyświetla kolejne wyrazy ciągu fibonacciego
+    funkcja zwraca
+
+
+### n-2+fn-1 dla n>1
+
+
+    a, b = (0,1)
+    if n == 0;
+        print a
+        return a
+    elif n == 1
+        print b
+        return b
+
+    for i in range(1, n)
+
+
+
+
+        return b
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
