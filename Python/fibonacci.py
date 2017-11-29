4c1c2199d8bbe63ff29282b8bdacbc8916ecbc1e
angelikaz09 <angelika904@wp.pl>
2017-11-29 12:36:15 +0100

    fibonacci

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -4,29 +4,47 @@
 
 def fib_iter(n):
     """funkcja wyświetla kolejne wyrazy ciągu fibonacciego
-    funkcja zwraca
+    funkcja zwraca n-ty wyraz ciągu
+    F(0) = 0
+    F(1) = 1
+    F(n) = F(n-2) + F(n-2) dla n > 1
+    """
 
-
-### n-2+fn-1 dla n>1
-
-
-    a, b = (0,1)
-    if n == 0;
-        print a
+    a, b = (0, 1)
+    if n == 0:
+        print (a)
         return a
-    elif n == 1
-        print b
-        return b
+    # elif n == 1:
+    #      print (b)
+    #      return b
 
-    for i in range(1, n)
+    print (a)
+    print (b)
+    for i in range(2, n):
+        # tmp = b
+        # b = a + b
+        # a = tmp
+        a, b = b, a + b
+        print (a, " wyraz ", i, ": ", b, "iloraz:", b / a)
+
+    return b
 
 
+def fib_iter2(n):
 
+    a, b = (0, 1)
 
-        return b
+    print (a)
+    while n > 0:
+         a, b = b, a + b
+        print (a, ": ", b, "iloraz:", b / a)
+        n = n - 1
+
+    return b
 
 
 def main(args):
+    fib_iter(10)
     return 0
 
 
---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+/*
+ * fibonacci.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int fib_iter(int n) {
+    int a = 0 
+    int b = 1
+    if (n == 0) }
+        cout << 0;
+        return a
+    {
+        cout <<a;
+        
+        for (int = 2; i++; i < n) 
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
