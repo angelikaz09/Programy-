c5e9ced211ddc00a05e94c609273eafbe9b0b98d
angelikaz09 <angelika904@wp.pl>
2018-02-28 11:58:02 +0100

    szyfr_Vigenere

--- Python/przestawieniowy.py
+++ Python/przestawieniowy.py
@@ -3,22 +3,43 @@
 
 
 def szyfruj(tekst, klucz):
-    reszta = len(tekst % klucz)
+    reszta = len(tekst) % klucz
+    szyfrogram = ""
+
     if reszta:
         tekst += (klucz - reszta) * "."
-        for i in range (klucz):
-    print()
 
-    return "."
+    for i in range(klucz):
+        for j in range(int(len(tekst) / klucz)):
+            # print(i + j * klucz, tekst[i + j * klucz])
+            szyfrogram += tekst[i + j * klucz]
+
+    return szyfrogram
+
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for i in range(int(len(szyfrogram) / klucz)):
+        for j in range(klucz):
+            tekst += szyfrogram[i + (j * int(len(szyfrogram) / klucz))]
+            tekst = tekst.replace(".", "")
+
+    return tekst
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
+
     klucz = int(input("Podaj klucz: "))
-    while 2 * (klucz > len(tekst)):
+    while (2 * klucz > len(tekst)):
+        klucz = int(input("Podaj klucz: "))
+
+
     szyfrogram = szyfruj(tekst, klucz)
-    print(szyfrogram)
-    # print(deszyfruj(szyfrogram, klucz))
+    print("Zaszyfrowany: ", szyfrogram)
+    deszyfrogram = deszyfruj(szyfrogram, klucz)
+    print("Deszyfrowany: ", deszyfrogram)
+
     return 0
 
 
